abce69d465aedfacd8571ec3de45b1e45b55a4cc
drzly777 <andrew.zheng725@gmail.com>
2017-05-09 11:59:19 -0400

    edited cli skeleton

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,16 +1,18 @@
 class CLI
 
-  #attr_reader :game
+  #attr_reader :user
   def initialize
     ##should this be a possibility? how will the data be kept after the user exits?
     #load Trainers.all each Trainer has pokemon stats and you can battle other pokemon and trainers at random
     #@game = Game.new
-    #choose to laod trainer or create a new one
+    #choose to load trainer or create a new one
   end
 
-  #def choose_or_create_game
-    #lets user choose a saved game or create a new one
-  #end
+  def load_or_create_game
+    #puts "Do you want to load a saved game or start a new one"
+    #if load then @user = picked user else @user = user.new
+  end
+
 
   def choose_action_group
     #1. Battle next opponent/trainer?
@@ -25,15 +27,17 @@ class CLI
   def battle_trainer
     #opposing_trainer = Opponent.new
     #battle_with_trainer = BattleTrainer.new(opponent_trainer)
+    #Battle
     ##find a trainer/populate with pokemon --- Pokemon.create_at_random(MyPokemon.avg_lvl)
     #1. pick move
     #2. pick another pokemon
+
   end
 
   #battle_wild_pokemon
   def battle_wild_pokemon
-    #wild_pokemon = Pokemon.new
-    #battle_with_pokemon = BattleWildPokemon.new(wild_pokemon)
+    #opponent_pokemon = Pokemon.new
+    #battle_with_pokemon = BattlPokemon.new(opponent_pokemon)
     #find a pokemon and populate stats
     #1. pick move
     #2. pick pokemon

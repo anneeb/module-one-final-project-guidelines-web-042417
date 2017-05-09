097af36283b9562ecfa77000f709a11ec5397707
drzly777 <andrew.zheng725@gmail.com>
2017-05-09 17:28:23 -0400

    added dummy test loops and commented play_out_battle functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -53,8 +53,10 @@ class CLI
   end
 
   def battle(user_pokemon = nil, opponent_pokemon = nil)
+    opponent_pokemon = opponent_pokemon
+    user_pokemon = user_pokemon
     opponent_pokemon |= Adapter.get_random_pokemon
-    user_pokemon |= @user.pokemon.first_not_fainted
+    user_pokemon |= @user.pokemon.first_not_fainted ##???
     puts "What would you like to do"
     puts "1. Attack #{opponent_pokemon.name}"
     puts "2. Switch to a different pokemon"
@@ -66,8 +68,10 @@ class CLI
     case user_choice
     when "1"
       poke_battle = BattlePokemon.new(user_pokemon, opponent_pokemon)
-      poke_battle.play_turn
-      battle(user_pokemon, opponent_pokemon)
+      #poke_battle.play_turn
+      #choose attack
+      play_out_battle(poke_battle)
+      #battle(user_pokemon, opponent_pokemon)
     when "2"
       switch_pkmn(opponent_pokemon)
     when "3"
@@ -81,8 +85,37 @@ class CLI
 
   end
 
+  def play_out_battle(battle)
+    puts "Which attack would you like to use?"
+    types = battle.get_user_types
+    types.each_with_index {|type, index| puts "#{index + 1}. #{type}"}
+    #puts "#{types.size + 1}. Back to battle menu"
+    user_type_choice = gets.chomp.to_i
+
+    #if user_type_choice == 1
+    # => battle.play_turn(0)
+    #elsif user_type_choice == 2 && types.size == 2
+    # => battle.play_turn(1)
+    #elsif user_type_choice == 2
+    # => battle(battle.user_pkmn, battle.opp_pkmn)
+    #elsif user_type_choice == 3 && types.size == 2
+    # => battle(battle.user_pkmn, battle.opp_pkmn)
+    #else
+    # => play_out_battle(battle)
+    #end
+    test_ongoing = false
+    if test_ongoing
+      play_out_battle
+    else
+      main_options
+    end
+
+  end
+
+
   def switch_pkmn(opponent_pokemon)
     puts "Choose the pokemon you want to use."
+    puts "This is part of a test. Code will break because user.available_pkmn methods haven't been implemented yet"
     available_pkmn = @user.available_pkmn# gets pokemon names @user.pokemon.reject {|pokemon| pokemon.fainted} #need to have a fainted status for each pokemon and put into User class?
     available_pkmn.each_with_index {|pokemon, index| puts "#{index + 1}. #{pokemon.name}"}
     pkmn_choice = gets.chomp.to_i
@@ -97,7 +130,8 @@ class CLI
   end
 
   def throw_pokeball(battle)
-    if battle.capture
+    test_capture = false
+    if test_capture#battle.capture
       puts "Congrats! You've captured #{opponent_pokemon.name}."
       captured_pokemon
     else

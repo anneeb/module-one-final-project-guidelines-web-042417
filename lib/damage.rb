a46ad4e35ec6a090d9b0d9786784ddd05e94e1c6
drzly777 <andrew.zheng725@gmail.com>
2017-05-10 10:27:25 -0400

    changed resistances hash to have a def_type and effect

--- lib/damage.rb
+++ lib/damage.rb
@@ -24,111 +24,111 @@ class Damage
         when "normal"
           case d_type
           when "rock"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           when "ghost"
-            res << {type: "no effect", mult: 0.0}
+            res << {type: d_type, effect: "no effect", mult: 0.0}
           end
         when "fire"
           case d_type
           when "grass", "ice", "bug"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "fire", "water", "rock", "dragon"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "water"
           case d_type
           when "fire", "ground", "rock"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "water", "grass", "dragon"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "electric"
           case
           when "water", "flying"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "electric", "grass", "dragon"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           when "ground"
-            res << {type: "no effect", mult: 0.0}
+            res << {type: d_type, effect: "no effect", mult: 0.0}
           end
         when "grass"
           case
           when "water", "ground", "rock"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "fire", "grass", "poison", "flying", "bug", "dragon"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "ice"
           case d_type
           when "grass", "ground", "flying", "dragon"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "water", "ice"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "fighting"
           case d_type
           when "normal", "ice", "rock"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "poison", "flying", "psychic", "bug", "rock"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           when "ghost"
-            res << {type: "no effect", mult: 0.0}
+            res << {type: d_type, effect: "no effect", mult: 0.0}
           end
         when "poison"
           case d_type
           when "grass", "bug"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "poison", "ground", "rock", "ghost"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "ground"
           case d_type
           when "fire", "electric", "poison", "rock"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "grass", "bug"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           when "flying"
-            res << {type: "no effect", mult: 0.0}
+            res << {type: d_type, effect: "no effect", mult: 0.0}
           end
         when "flying"
           case d_type
           when "grass", "fighting", "bug"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "electric", "rock"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "psychic"
           case d_type
           when "fighting", "poison"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "psychic"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "bug"
           case d_type
           when "grass", "poison", "psychic"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "fire", "fighting", "flying"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "rock"
           case d_type
           when "fire", "ice", "flying", "bug"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "fighing", "ground"
-            res << {type: "not very effective", mult: 0.5}
+            res << {type: d_type, effect: "not very effective", mult: 0.5}
           end
         when "ghost"
           case d_type
           when "ghost"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           when "normal", "psychic"
-            res << {type: "no effect", mult: 0.0}
+            res << {type: d_type, effect: "no effect", mult: 0.0}
           end
         when "dragon"
           case d_type
           when "dragon"
-            res << {type: "super effective", mult: 2.0}
+            res << {type: d_type, effect: "super effective", mult: 2.0}
           end
         end
       end
